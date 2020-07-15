2550a1a18a6ab0ba70959e851f75d5b5379c0e00
Clover Chapman <clo@cloverseeds.co.uk>
2020-07-15 22:16:06 +0100

    implement tabs click handler

--- tabs.js
+++ tabs.js
@@ -1,1 +1,35 @@
-console.log('log from tabs.js');
+document.addEventListener("DOMContentLoaded", function (event) {
+    const tab = document.querySelector('.tab-component');
+    const tabButtons = tab.querySelectorAll('[role="tab"]');
+    const tabPanels = Array.from(tab.querySelectorAll('[role="tabpanel"]'));
+
+    function tabClickHandler(e) {
+        //hide all tabPanels
+        tabPanels.forEach(panel => {
+            panel.hidden = 'true';
+        });
+
+        //deselect tab
+        tabButtons.forEach(button => {
+            button.setAttribute('aria-selected', 'false');
+        });
+
+        //mark new tab as selected
+        e.currentTarget.setAttribute('aria-selected', 'true');
+
+        //show new Tab
+        const {
+            id
+        } = e.currentTarget;
+        console.log(id);
+
+        const currentTab = tabPanels.find(
+            panel => panel.getAttribute('aria-labelledby') === id
+        );
+        currentTab.hidden = false;
+    }
+
+    tabButtons.forEach(button => {
+        button.addEventListener('click', tabClickHandler);
+    });
+});
