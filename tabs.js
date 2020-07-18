1c88e4d9829c156e5fdcfa715aeb80a493351f0b
Clover Chapman <clo@cloverseeds.co.uk>
2020-07-18 13:42:44 +0100

    refactor tab click handler in to seperate functions

--- tabs.js
+++ tabs.js
@@ -3,30 +3,60 @@ document.addEventListener("DOMContentLoaded", function (event) {
     const tabButtons = tab.querySelectorAll('[role="tab"]');
     const tabPanels = Array.from(tab.querySelectorAll('[role="tabpanel"]'));
 
-    function tabClickHandler(e) {
+    function tabClickHandler(event) {
         //hide all tabPanels
-        tabPanels.forEach(panel => {
-            panel.hidden = 'true';
-        });
+        // tabPanels.forEach(panel => {
+        //     panel.hidden = 'true';
+        // });
+
+        hideTabs();
+
+        showTabs(event);
 
         //deselect tab
-        tabButtons.forEach(button => {
-            button.setAttribute('aria-selected', 'false');
-        });
+        // tabButtons.forEach(button => {
+        //     button.setAttribute('aria-selected', false);
+        // });
 
-        //mark new tab as selected
-        e.currentTarget.setAttribute('aria-selected', 'true');
+        // //mark new tab as selected
+        // event.currentTarget.setAttribute('aria-selected', 'true');
+
+        // //show new Tab
+        // const {
+        //     id
+        // } = event.currentTarget;
+
+        // const currentTab = tabPanels.find(
+        //     panel => panel.getAttribute('aria-labelledby') === id
+        // );
+        // currentTab.hidden = false;
+    }
+
+    const hideTabs = () => {
+        console.log("I'm being hidden");
+        tabButtons.forEach(button => {
+            button.setAttribute('aria-selected', false);
+        });
+        tabPanels.forEach(panel => {
+            panel.hidden = true;
+        });
+    }
+
+    const showTabs = event => {
+        event.currentTarget.setAttribute('aria-selected', true);
 
         //show new Tab
         const {
             id
-        } = e.currentTarget;
+        } = event.currentTarget;
 
         const currentTab = tabPanels.find(
             panel => panel.getAttribute('aria-labelledby') === id
         );
         currentTab.hidden = false;
     }
+
+
 
     tabButtons.forEach(button => {
         button.addEventListener('click', tabClickHandler);
